d11a3d876eb30dba8c981317ef3a80227efda5d7
kinoshitaken123 <k1k7s9e3@gmail.com>
2021-05-25 22:28:07 +0900

    [update]

--- practice.js
+++ practice.js
@@ -1,5 +1,10 @@
-function sayHello() {
-  alert("こんにちは");
-}
-var F = sayHello;
-F();
+var obj1 = {
+  name: "よしお",
+  greet: function () {
+    console.log(this.name + "です、初めまして");
+  }
+};
+var obj2 = {
+  name: "百合子"
+};
+obj1.greet.call(obj2);
